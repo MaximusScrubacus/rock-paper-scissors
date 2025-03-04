4e37478584083cfd8038880111c1fa80bc330ef2
maximusscrubacus <199446646+MaximusScrubacus@users.noreply.github.com>
2025-03-04 19:05:55 +1100

    index.js

--- index.js
+++ index.js
@@ -26,21 +26,45 @@ let computerChoiceReadout = computerChoice();
 function computerReadout(){
     return "Computer Chose" + " " + computerChoiceReadout;
 }
-let computerRoll = computerReadout();
-console.log(computerRoll)
+let computerRoll = computerChoiceReadout
+let computerRolls = computerReadout();
+console.log(computerRolls)
 
-
-
+// Round Win Function
 
 function roundWinner(){
-    if (playerChoice == computerChoice){
-       return "Draw";
-    }else{
-        return "You lose this round";
+    if (playerChoice === "rock" && computerRoll === "Scissors"){
+       return "Player Wins!";} 
+       else if(playerChoice === "rock" && computerRoll === "Paper"){
+        return "You lose this round!, try again !!!";
+       } 
+       else if(playerChoice === "rock" && computerRoll === "Rock"){
+        return "Draw!"
+       } 
+       else if (playerChoice === "paper" && computerRoll === "Rock"){
+        return "Player Wins!";} 
+        else if(playerChoice === "paper" && computerRoll === "Scissors"){
+         return "You lose this round!, try again !!!"
+        }
+        else if(playerChoice === "paper" && computerRoll === "Paper"){
+         return "Draw!"
+        }
+        else if (playerChoice === "scissors" && computerRoll === "Paper"){
+            return "Player Wins!";} 
+            else if(playerChoice === "scissors" && computerRoll === "Rock"){
+             return "You lose this round!, try again !!!"
+            }
+            else if(playerChoice === "scissors" && computerRoll === "Scissors"){
+             return "Draw!"
+            }
+            else{
+                return "Try Again!"
+            }
     }
-}
-let roundWin = roundWinner();
 
-console.log(roundWinner())
-// alert(roundWin);
+let roundWin = roundWinner();
+console.log(roundWin)
+// let roundWin = roundWinner();
+
+// console.log(roundWinner())
  
